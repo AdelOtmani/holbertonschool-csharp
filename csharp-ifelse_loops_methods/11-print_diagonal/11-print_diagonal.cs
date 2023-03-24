415c52d3076dd3ff4836d02528d8a7a6b8d2870c
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-03-24 23:18:08 +0100

    exercice 10 return line

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < lenght; i++)
+        {
+            for (int j = 0; j <= 0; j++)
+            {
+                Console.Write("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
