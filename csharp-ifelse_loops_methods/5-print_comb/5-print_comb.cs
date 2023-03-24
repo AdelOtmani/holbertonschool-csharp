37db4b80380f8547349b4210cf487d7447a93fbc
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-03-24 22:13:51 +0100

    exercice 5 print digit

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,8 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int digit = 0; digit < 99; digit++){
-            Console.WriteLine("{0:00}",digit);
+        for (int digit = 0; digit <= 99; digit++){
+            if (digit == 99){
+                Console.Write("{0:00}",digit);
+            }
+            else{
+                Console.Write("{0:00}, ",digit);
+            }
         } 
     }
 }
