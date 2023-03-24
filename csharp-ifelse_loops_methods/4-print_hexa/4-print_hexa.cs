b1240b0124faa20eecd33cd142b52bfebde8cfb6
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-03-24 21:58:18 +0100

    exercice 4 print hexa

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         for (int hexa = 0; hexa < 99; hexa++){
-            Console.WriteLine("{0} = 0x{0:X}",hexa);
+            Console.WriteLine("{0} = 0x{0:x}",hexa);
         } 
     }
 }
