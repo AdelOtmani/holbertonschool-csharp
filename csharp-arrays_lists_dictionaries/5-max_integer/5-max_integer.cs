662112b3d6773d306c3970e9990ba579aedcc9eb
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-03-30 22:08:05 +0200

    project 1 exercise 0 to 5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,23 @@
+using System;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        int Max;
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty")
+            return (-1);
+        }
+        Max = myList[0];
+        foreach (var elem in myList)
+        {
+            if (elem > Max)
+            {
+                Max = elem;
+            }
+        }
+        return (Max);
+    }
+}
