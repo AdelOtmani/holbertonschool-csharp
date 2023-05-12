12add701a8b923cf6cb3efb704f570da9b523b09
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-12 20:27:45 +0200

    task 9

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -10,17 +10,17 @@ class VectorMath
         if (vector.Length == 2)
         {
             double[] res = { 0, 0 };
-            res[0] = vector1[0] * scalar;
-            res[1] = vector1[1] * scalar;
+            res[0] = vector[0] * scalar;
+            res[1] = vector[1] * scalar;
 
             return (res);
         }
         else if (vector.Length == 3)
         {
             double[] res = { 0, 0, 0 };
-            res[0] = vector1[0] * scalar;
-            res[1] = vector1[1] * scalar;
-            res[2] = vector1[2] * scalar;
+            res[0] = vector[0] * scalar;
+            res[1] = vector[1] * scalar;
+            res[2] = vector[2] * scalar;
 
             return (res);
         }
