016437b4daef8a279b59855e725d1a60ec4d664a
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-12 18:29:09 +0200

    Task 3

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,27 @@
+using System;
+using system. Collections.Generic;
+
+class VectorMath
+{
+
+    ///<summary> method that calculates and returns the length of a given vector.</summary>
+    public static double Magnitude(double[] vector)
+    {
+        c = vector.Count();
+        res = 0;
+
+        if (c == 2)
+        {
+            res = sqrt((vector[0]**2) + (vector[1]**2));
+        }
+        else if (c == 3)
+        {
+            res = sqrt((vector[0]**2) + (vector[1]**2) + (vector[2]**2));
+        }
+        else
+        {
+            return (-1);
+        }
+        return(res);
+    }
+}
