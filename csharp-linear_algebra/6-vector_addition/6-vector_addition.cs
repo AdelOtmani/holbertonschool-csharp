33a330995926f4a49dda37d59d8d4d153d559447
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-12 20:08:14 +0200

    Task 5

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+
+    ///<summary> method that adds two vectors and returns the resulting vector.</summary>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] res;
+        if (vector1.Length == 2) and (vector2.Length == 2)
+        {
+            res = {0, 0};
+            res[0] = vector1[0] + vector2[0];
+            res[1] = vector1[1] + vector2[1];
+
+            return (res);
+        }
+        else if (vector1.Length == 3) and (vector2.Length == 3)
+        {
+            res = {0, 0, 0};
+            res[0] = vector1[0] + vector2[0];
+            res[1] = vector1[1] + vector2[1];
+            res[2] = vector1[2] + vector2[2];
+
+            return (res);
+        }
+        else
+        {
+            return new double[] {-1};
+        }
+
+    }
+}
