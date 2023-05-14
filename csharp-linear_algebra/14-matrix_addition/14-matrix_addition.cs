8d972742271bc00c0491ce72356c295d1296331b
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-14 02:38:47 +0200

    task 14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -17,7 +17,7 @@ class MatrixMath
 
             return (res);
         }
-        if ((matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 && matrix1.GetLength(2) == 3) && (matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3 && matrix2.GetLength(2) == 3))
+        if ((matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3) && (matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3))
         {
             double[,] res = {{ 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }};
             res[0, 0] = matrix1[0, 0] + matrix2[0, 0];
