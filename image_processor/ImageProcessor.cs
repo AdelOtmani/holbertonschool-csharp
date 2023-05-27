c428f4cd221d22272216f6e96b62f8fd68f56f84
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-27 20:06:03 +0200

    Task 0

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Drawing;
 
 namespace image_processor
 {
@@ -7,7 +8,34 @@ namespace image_processor
     {
         public static void Inverse(string[] filenames)
         {
-            Console.WriteLine("Hello World!");
+            foreach (string filename in filenames)
+            {
+                // Load the image file
+                Bitmap originalImage = new Bitmap(filename);
+
+                // Create a new bitmap with the same dimensions as the original image
+                Bitmap invertedImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+                // Iterate over each pixel in the image
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    for (int x = 0; x < originalImage.Width; x++)
+                    {
+                        // Get the color of the current pixel
+                        Color pixelColor = originalImage.GetPixel(x, y);
+
+                        // Calculate the inverted color by subtracting each color component from 255
+                        Color invertedColor = Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B);
+
+                        // Set the inverted color for the corresponding pixel in the new image
+                        invertedImage.SetPixel(x, y, invertedColor);
+                    }
+                }
+
+                // Save the inverted image to a file with "_inverted" appended to the original filename
+                string invertedFilename = filename.Insert(filename.LastIndexOf('.'), "_inverted");
+                invertedImage.Save(invertedFilename);
+            }
         }
     }
 }
