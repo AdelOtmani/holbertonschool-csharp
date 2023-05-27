2cea26100cc16bc8ed73582a09e055b17a9aef44
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-27 20:00:19 +0200

    cs root

---
+++ image_processor/ImageProcessor.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace image_processor
+{
+    class ImageProcessor
+    {
+        public static void Inverse(string[] filenames)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
