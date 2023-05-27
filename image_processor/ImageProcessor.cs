9637d381700124a60b5f18d45421fe55eca027aa
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-27 20:29:04 +0200

    Task 0

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,41 +1,47 @@
 using System;
 using System.IO;
 using System.Drawing;
+using System.Threading.Tasks;
 
-namespace image_processor
+class ImageProcessor
 {
-    class ImageProcessor
+    public static void Inverse(string[] filenames)
     {
-        public static void Inverse(string[] filenames)
+        foreach (string filename in filenames)
         {
-            foreach (string filename in filenames)
+            // Load the image file
+            Bitmap originalImage = new Bitmap(filename);
+
+            // Create a new bitmap with the same dimensions as the original image
+            Bitmap invertedImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int y = 0; y < originalImage.Height; y++)
             {
-                // Load the image file
-                Bitmap originalImage = new Bitmap(filename);
-
-                // Create a new bitmap with the same dimensions as the original image
-                Bitmap invertedImage = new Bitmap(originalImage.Width, originalImage.Height);
-
-                // Iterate over each pixel in the image
-                for (int y = 0; y < originalImage.Height; y++)
+                for (int x = 0; x < originalImage.Width; x++)
                 {
-                    for (int x = 0; x < originalImage.Width; x++)
-                    {
-                        // Get the color of the current pixel
-                        Color pixelColor = originalImage.GetPixel(x, y);
-
-                        // Calculate the inverted color by subtracting each color component from 255
-                        Color invertedColor = Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B);
-
-                        // Set the inverted color for the corresponding pixel in the new image
-                        invertedImage.SetPixel(x, y, invertedColor);
-                    }
+                    Color pixelColor = originalImage.GetPixel(x, y);
+                    Color invertedColor = Color.FromArgb(
+                        255 - pixelColor.R,
+                        255 - pixelColor.G,
+                        255 - pixelColor.B
+                    );
+                    invertedImage.SetPixel(x, y, invertedColor);
                 }
-
-                // Save the inverted image to a file with "_inverted" appended to the original filename
-                string invertedFilename = filename.Insert(filename.LastIndexOf('.'), "_inverted");
-                invertedImage.Save(invertedFilename);
             }
+
+            // Get the original file extension
+            string extension = Path.GetExtension(filename);
+
+            // Create the new filename
+            string invertedFilename = $"{Path.GetFileNameWithoutExtension(filename)}_inverse{extension}";
+
+            // Save the inverted image to the application's root directory
+            string savePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, invertedFilename);
+            invertedImage.Save(savePath);
+
+            // Dispose of the image objects to free up memory
+            originalImage.Dispose();
+            invertedImage.Dispose();
         }
     }
 }
