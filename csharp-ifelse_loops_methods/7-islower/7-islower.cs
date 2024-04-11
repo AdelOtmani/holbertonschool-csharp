a5a727c1e584ef05b2c24d0b96888e7f1835011c
AdelOtmani <5023@holbertonschool.com>
2024-04-11 11:45:31 +0200

    Task 7 Done

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (ord(c) >= 97 && ord(c) <= 122){
+            return (True);
+        }
+        else{
+            return (False);
+        }
+
+    }
+}
