ac572ffa688e20d91ed57b190679a0792565759a
AdelOtmani <5023@holbertonschool.com>
2024-04-11 11:52:32 +0200

    Task 11 Done

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        for (int i = 0; i < lenght; i++){
+            for (int j = 0; j <= 0; j++)
+            {
+                Console.Write("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
