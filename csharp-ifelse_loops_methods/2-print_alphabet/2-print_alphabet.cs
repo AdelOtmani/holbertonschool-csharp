9d2b80d44e7ede4cd7b075435a0955ea91c29e30
AdelOtmani <5023@holbertonschool.com>
2024-04-11 11:38:27 +0200

    Task 2 Done

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter != '{'; letter++){
+            Console.Write(letter);
+        }
+    }
+}
