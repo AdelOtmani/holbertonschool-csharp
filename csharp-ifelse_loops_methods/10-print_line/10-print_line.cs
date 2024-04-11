2a9bd96181bc3a0c07ac5de13f64eba8a2ccd1bb
AdelOtmani <5023@holbertonschool.com>
2024-04-11 11:49:47 +0200

    Task 10 Done

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (lenght == 0){
+            Console.WriteLine("");
+        }
+        for (int i = 0; i == 3; i++){
+            Console.Write('_');
+        }
+    }
+}
