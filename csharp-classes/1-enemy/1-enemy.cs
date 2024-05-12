15aa84d1ebe3d67fca7c1d0a17937b2409bc2a40
AdelOtmani <5023@holbertonschool.com>
2024-05-12 14:35:12 +0200

    Taskes 1 to 3 Done (worked on local)  and Readme update

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Enemies
+{
+
+    ///<summary>Class Zombie. Defines health and a construct set to 0</summary>
+    public class Zombie
+    {
+        ///<summary>Health field</summary>
+        public int health;
+
+        ///<summary>Public contructor <see cref="Zombie"/> class</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}
